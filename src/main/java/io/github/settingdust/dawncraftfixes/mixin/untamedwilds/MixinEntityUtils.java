85ec9e86c899b6443b2763a74479dbeea4d282f7
SettingDust <settingdust@gmail.com>
2023-05-21 11:16:22 +0800

    fix(untamedwilds): check the variant exist
    https://github.com/RayTrace082/untamedwilds/issues/210
    https://github.com/RayTrace082/untamedwilds/issues/206

--- src/main/java/io/github/settingdust/dawncraftfixes/mixin/untamedwilds/MixinEntityUtils.java
+++ src/main/java/io/github/settingdust/dawncraftfixes/mixin/untamedwilds/MixinEntityUtils.java
@@ -1,11 +1,21 @@
 package io.github.settingdust.dawncraftfixes.mixin.untamedwilds;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import untamedwilds.entity.ComplexMob;
 import untamedwilds.util.EntityUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Mixin(EntityUtils.class)
 public class MixinEntityUtils {
@@ -18,5 +28,15 @@ public class MixinEntityUtils {
     @ModifyConstant(method = "dropEggs", constant = @Constant(stringValue = "custom_model_data"), remap = false)
     private static String dcfixes$wrongEggTag(String constant) {
         return "CustomModelData";
+    }
+
+    @WrapOperation(method = "getSkinFromEntity", remap = false, at = @At(value = "INVOKE", target = "Ljava/util/HashMap;containsKey(Ljava/lang/Object;)Z", ordinal = 0))
+    private static boolean dcfixes$rareTextureNotExist(HashMap<String, HashMap<Integer, ArrayList<ResourceLocation>>> instance, Object variant, Operation<Boolean> original, @Local String name) {
+        return instance.containsKey(name) && original.call(instance, variant);
+    }
+
+    @WrapOperation(method = "getSkinFromEntity", remap = false, at = @At(value = "INVOKE", target = "Ljava/util/HashMap;containsKey(Ljava/lang/Object;)Z", ordinal = 1))
+    private static boolean dcfixes$commonTextureNotExist(HashMap<String, HashMap<Integer, ArrayList<ResourceLocation>>> instance, Object variant, Operation<Boolean> original, @Local String name) {
+        return instance.containsKey(name) && original.call(instance, variant);
     }
 }
