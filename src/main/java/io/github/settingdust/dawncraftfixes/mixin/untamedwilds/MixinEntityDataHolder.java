ec026e87de353d4b08003fc988070556106a4355
SettingDust <settingdust@gmail.com>
2023-05-21 10:42:20 +0800

    fix(untamedwilds): simple fix, may cause other problem. but it's fine
    https://github.com/RayTrace082/untamedwilds/issues/233

---
+++ src/main/java/io/github/settingdust/dawncraftfixes/mixin/untamedwilds/MixinEntityDataHolder.java
@@ -0,0 +1,25 @@
+package io.github.settingdust.dawncraftfixes.mixin.untamedwilds;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import untamedwilds.util.EntityDataHolder;
+import untamedwilds.util.SpeciesDataHolder;
+
+import java.util.List;
+
+@Mixin(EntityDataHolder.class)
+public class MixinEntityDataHolder {
+    @Shadow
+    @Final
+    private List<SpeciesDataHolder> speciesData;
+
+    @Inject(method = "getName", at = @At("HEAD"), remap = false, cancellable = true)
+    public void dcfixes$getName(int i, CallbackInfoReturnable<String> cir) {
+        if (speciesData.size() <= i)
+            cir.setReturnValue("");
+    }
+}
