c2b69dc723b5df64e3bd81b041a4719169f93aea
PierKnight100 <pier.altimari@libero.it>
2023-05-29 22:33:30 +0100

    Fixed Epic Fight Client Check, not the cloud chest search bar works correctly

---
+++ src/main/java/io/github/settingdust/dawncraftfixes/mixin/epicfight/MixingRenderEngineEvents.java
@@ -0,0 +1,20 @@
+package io.github.settingdust.dawncraftfixes.mixin.epicfight;
+
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import yesman.epicfight.client.events.engine.RenderEngine;
+
+import java.util.Objects;
+
+@Mixin(RenderEngine.Events.class)
+public class MixingRenderEngineEvents
+{
+    @Inject(remap = false, method = "itemTooltip",at = @At("HEAD"), cancellable = true)
+    private static void dcfixes$wrongNameOfSetSwag(ItemTooltipEvent tooltipEvent, CallbackInfo callbackInfo) {
+        if(!Objects.requireNonNull(tooltipEvent.getPlayer()).getLevel().isClientSide)
+            callbackInfo.cancel();
+    }
+}
