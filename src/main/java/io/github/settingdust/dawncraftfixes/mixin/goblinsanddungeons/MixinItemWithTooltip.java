91479873e2987d40366d16a0c7c0cae8b5bf92b2
SettingDust <settingdust@gmail.com>
2023-06-16 13:34:36 +0800

    fix(goblinsanddungeons): avoid crash by no level when start up build search index

--- src/main/java/io/github/settingdust/dawncraftfixes/mixin/goblinsanddungeons/MixinItemWithTooltip.java
+++ src/main/java/io/github/settingdust/dawncraftfixes/mixin/goblinsanddungeons/MixinItemWithTooltip.java
@@ -1,36 +1,37 @@
 package io.github.settingdust.dawncraftfixes.mixin.goblinsanddungeons;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import superlord.goblinsanddungeons.item.GoblinCrownItem;
 import superlord.goblinsanddungeons.item.HealthRingItem;
 import superlord.goblinsanddungeons.item.StealthRingItem;
 
+import javax.annotation.Nullable;
 import java.util.List;
 
 @Mixin({StealthRingItem.class, HealthRingItem.class, GoblinCrownItem.class})
 public class MixinItemWithTooltip {
-    @Inject(
+    @WrapOperation(
             method = "appendHoverText",
             at =
                     @At(
                             value = "INVOKE",
-                            target =
-                                    "Lnet/minecraft/world/item/ArmorItem;appendHoverText(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/Level;Ljava/util/List;Lnet/minecraft/world/item/TooltipFlag;)V",
-                            shift = At.Shift.AFTER),
-            cancellable = true)
-    private void dcfixes$ensureServer(
+                            target = "Lcom/mojang/blaze3d/platform/InputConstants;isKeyDown(JI)Z",
+                            ordinal = 0))
+    private boolean dcfixes$ensureServer(
+            long window,
+            int key,
+            Operation<Boolean> original,
             ItemStack p_77624_1_,
-            Level p_77624_2_,
+            @Nullable Level level,
             List<Component> p_77624_3_,
-            TooltipFlag p_77624_4_,
-            CallbackInfo ci) {
-        if (!p_77624_2_.isClientSide) ci.cancel();
+            TooltipFlag p_77624_4_) {
+        return level != null && !level.isClientSide && original.call(p_77624_1_, level, p_77624_3_, p_77624_4_);
     }
 }
