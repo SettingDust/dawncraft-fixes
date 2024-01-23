d9831ed8d45714bfd55f43846bfd6e1298387723
SettingDust <settingdust@gmail.com>
2024-01-23 12:01:42 +0800

    feat(citadel): disable network in main thread. Accelerated startup

---
+++ mod/src/main/java/io/github/settingdust/dawncraftfixes/mixin/citadel/CitadelMixin.java
@@ -0,0 +1,28 @@
+package io.github.settingdust.dawncraftfixes.mixin.citadel;
+
+import com.github.alexthe666.citadel.Citadel;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.io.BufferedReader;
+
+@Mixin(value = Citadel.class, remap = false)
+public class CitadelMixin {
+    @WrapOperation(
+            method = "setup",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lcom/github/alexthe666/citadel/web/WebHelper;getURLContents(Ljava/lang/String;Ljava/lang/String;)Ljava/io/BufferedReader;"))
+    private BufferedReader dcfixes$disableNetwork(
+            final String url,
+            final String connection,
+            final Operation<BufferedReader> original) {
+        return null;
+    }
+}
