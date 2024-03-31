679c665fd13e597ac2959c720b2e9e31a4481aa5
SettingDust <settingdust@gmail.com>
2024-03-31 21:21:55 +0800

    feat(pmmo): only disable the tooltip when building rei cache

---
+++ mod/src/main/java/io/github/settingdust/dawncraftfixes/mixin/rei/TooltipArgumentTypeMixin.java
@@ -0,0 +1,28 @@
+package io.github.settingdust.dawncraftfixes.mixin.rei;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import harmonised.pmmo.client.events.TooltipHandler;
+import me.shedaniel.rei.api.common.entry.EntryStack;
+import me.shedaniel.rei.impl.client.search.argument.type.TooltipArgumentType;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(TooltipArgumentType.class)
+public class TooltipArgumentTypeMixin {
+    @WrapOperation(
+            method = "cacheData(Lme/shedaniel/rei/api/common/entry/EntryStack;)Ljava/lang/String;",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lme/shedaniel/rei/impl/client/search/argument/type/TooltipArgumentType;tryGetEntryStackTooltip(Lme/shedaniel/rei/api/common/entry/EntryStack;I)Ljava/lang/String;"))
+    private @Nullable String dcfixes$avoidPMMOTooltip(
+            final EntryStack<?> entry, final int joiner, final Operation<String> original) {
+        TooltipHandler.tooltipOn = false;
+        var result = original.call(entry, joiner);
+        TooltipHandler.tooltipOn = true;
+        return result;
+    }
+}
